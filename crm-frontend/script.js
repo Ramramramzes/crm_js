7ee8085411e544dc006fd86d5f734984febe355b
Marat <68328146+Ramramramzes@users.noreply.github.com>
2024-04-10 16:20:40 +0300

    id filter btn

--- crm-frontend/script.js
+++ crm-frontend/script.js
@@ -1,141 +1,160 @@
 const list = document.getElementById('list'); //? Основной список
-
+let filterIdFlag = false;
 (async () => {
   try {
-    const allClients = await getClients();
-    if (allClients.length !== 0) {
-      for (let i = 0; i < allClients.length; i++) {
-        const item = document.createElement('li');
-        item.classList.add('list_item')
-        const idDiv = document.createElement('div');
-        idDiv.classList.add('id');
-        idDiv.textContent =  `${allClients[i].id}`;
+    
+    let allClients = await getClients();
+    renderList(allClients)
+    const filterId = document.getElementById('filter_id')
+    filterId.addEventListener('click',() => {
+      if(!filterIdFlag){
+        allClients = allClients.sort((a, b) => parseInt(b.id) - parseInt(a.id));
+        list.innerHTML = ''
+        filterIdFlag = !filterIdFlag
+        renderList(allClients)
+      }else{
+        allClients = allClients.sort((a, b) => parseInt(a.id) - parseInt(b.id));
+        list.innerHTML = ''
+        filterIdFlag = !filterIdFlag
+        renderList(allClients)
+      }
+    })
 
-        const fioDiv = document.createElement('div');
-        fioDiv.classList.add('fio');
-        fioDiv.textContent = `${allClients[i].surname} ${allClients[i].name} ${allClients[i].lastName}` ;
-
-        const createDateDiv = document.createElement('div');
-        createDateDiv.classList.add('create_date');
-        const createdDate = document.createElement('span')
-        const createdTime = document.createElement('span')
-        createdDate.textContent = formatedDateD(`${allClients[i].createdAt}`);
-        createdTime.textContent = formatedDateT(`${allClients[i].createdAt}`);
-        createDateDiv.append(createdDate,createdTime)
-
-        const changeDateDiv = document.createElement('div');
-        changeDateDiv.classList.add('change_date');
-        const changeDate = document.createElement('span')
-        const changeTime = document.createElement('span')
-        changeDate.textContent = formatedDateD(`${allClients[i].updatedAt}`);
-        changeTime.textContent = formatedDateT(`${allClients[i].updatedAt}`);
-        changeDateDiv.append(changeDate,changeTime)
-
-        const contactsDiv = document.createElement('div');
-        contactsDiv.classList.add('contacts');
-
-        for (let j = 0; j < allClients[i].contacts.length; j++) {
-          const contact_img_block = document.createElement('div')
-          contact_img_block.classList.add('contact_img_block')
-          const img = document.createElement('img')
-          contact_img_block.append(img)
-          img.classList.add(`contact_img_${j}`)
-          if(allClients[i].contacts[j].type === 'Facebook'){
-            img.src = './img/fb.svg'
-          }else
-          if(allClients[i].contacts[j].type === 'Телефон'){
-            img.src = './img/phone.svg'
-          }else
-          if(allClients[i].contacts[j].type === 'Email'){
-            img.src = './img/mail.svg'
-          }else
-          if(allClients[i].contacts[j].type === 'VK'){
-            img.src = './img/vk.svg'
-          }else
-          if(allClients[i].contacts[j].type === 'Другое'){
-            img.src = './img/other.svg'
-          }
-          if(j >= 4){
-            img.classList.add('dn')
-          }
-          
-          const content = document.createElement('div')
-          content.classList.add('title_of_contacts')
-          content.textContent = `${allClients[i].contacts[j].value}`
-          const retangle = document.createElement('div')
-          retangle.classList.add('hover_retangle','test')
-          content.append(retangle)
-
-          contact_img_block.append(content)
-          
-          content.classList.add('dn')
-          contact_img_block.addEventListener('mouseenter',() => {
-            content.classList.remove('dn')
-          })
-          contact_img_block.addEventListener('mouseleave',() => {
+    function renderList(allClients){
+      if (allClients.length !== 0) {
+        for (let i = 0; i < allClients.length; i++) {
+          const item = document.createElement('li');
+          item.classList.add('list_item')
+          const idDiv = document.createElement('div');
+          idDiv.classList.add('id');
+          idDiv.textContent =  `${allClients[i].id}`;
+  
+          const fioDiv = document.createElement('div');
+          fioDiv.classList.add('fio');
+          fioDiv.textContent = `${allClients[i].surname} ${allClients[i].name} ${allClients[i].lastName}` ;
+  
+          const createDateDiv = document.createElement('div');
+          createDateDiv.classList.add('create_date');
+          const createdDate = document.createElement('span')
+          const createdTime = document.createElement('span')
+          createdDate.textContent = formatedDateD(`${allClients[i].createdAt}`);
+          createdTime.textContent = formatedDateT(`${allClients[i].createdAt}`);
+          createDateDiv.append(createdDate,createdTime)
+  
+          const changeDateDiv = document.createElement('div');
+          changeDateDiv.classList.add('change_date');
+          const changeDate = document.createElement('span')
+          const changeTime = document.createElement('span')
+          changeDate.textContent = formatedDateD(`${allClients[i].updatedAt}`);
+          changeTime.textContent = formatedDateT(`${allClients[i].updatedAt}`);
+          changeDateDiv.append(changeDate,changeTime)
+  
+          const contactsDiv = document.createElement('div');
+          contactsDiv.classList.add('contacts');
+  
+          for (let j = 0; j < allClients[i].contacts.length; j++) {
+            const contact_img_block = document.createElement('div')
+            contact_img_block.classList.add('contact_img_block')
+            const img = document.createElement('img')
+            contact_img_block.append(img)
+            img.classList.add(`contact_img_${j}`)
+            if(allClients[i].contacts[j].type === 'Facebook'){
+              img.src = './img/fb.svg'
+            }else
+            if(allClients[i].contacts[j].type === 'Телефон'){
+              img.src = './img/phone.svg'
+            }else
+            if(allClients[i].contacts[j].type === 'Email'){
+              img.src = './img/mail.svg'
+            }else
+            if(allClients[i].contacts[j].type === 'VK'){
+              img.src = './img/vk.svg'
+            }else
+            if(allClients[i].contacts[j].type === 'Другое'){
+              img.src = './img/other.svg'
+            }
+            if(j >= 4){
+              img.classList.add('dn')
+            }
+            
+            const content = document.createElement('div')
+            content.classList.add('title_of_contacts')
+            content.textContent = `${allClients[i].contacts[j].value}`
+            const retangle = document.createElement('div')
+            retangle.classList.add('hover_retangle','test')
+            content.append(retangle)
+  
+            contact_img_block.append(content)
+            
             content.classList.add('dn')
-          })
-          contactsDiv.append(contact_img_block)
-        }
-        
-        const actionsDiv = document.createElement('div');
-        const changeUser = document.createElement('button')
-        const deleteUser = document.createElement('button')
-        changeUser.textContent = 'Изменить'
-        changeUser.classList.add('change_btn')
-        deleteUser.textContent = 'Удалить'
-        deleteUser.classList.add('del_btn')
-        actionsDiv.append(changeUser)
-        actionsDiv.append(deleteUser)
-        actionsDiv.classList.add('actions');
-        item.appendChild(idDiv);
-        item.appendChild(fioDiv);
-        item.appendChild(createDateDiv);
-        item.appendChild(changeDateDiv);
-        item.appendChild(contactsDiv);
-        item.appendChild(actionsDiv);
-
-        changeUser.addEventListener('click',() => {
-          changeUserFn(allClients[i].id)
-          document.getElementById('cancle_add').classList.add('dn')
-          document.getElementById('del_user_add').classList.remove('dn')
-          document.getElementById('popup_title').textContent = `Изменить данные `
-          document.getElementById('for_id').textContent = `ID: ${allClients[i].id}`
-          if(document.querySelectorAll('.contact_block_add').length <= 9){
-            document.getElementById('add_contact').classList.remove('dn')
+            contact_img_block.addEventListener('mouseenter',() => {
+              content.classList.remove('dn')
+            })
+            contact_img_block.addEventListener('mouseleave',() => {
+              content.classList.add('dn')
+            })
+            contactsDiv.append(contact_img_block)
           }
-        })
-
-        deleteUser.addEventListener('click',() => {
-          delPopup.classList.remove('dn')
-          document.getElementById('del_btn').addEventListener('click',() => {
-            deleteUserFromBase(allClients[i].id)
+          
+          const actionsDiv = document.createElement('div');
+          const changeUser = document.createElement('button')
+          const deleteUser = document.createElement('button')
+          changeUser.textContent = 'Изменить'
+          changeUser.classList.add('change_btn')
+          deleteUser.textContent = 'Удалить'
+          deleteUser.classList.add('del_btn')
+          actionsDiv.append(changeUser)
+          actionsDiv.append(deleteUser)
+          actionsDiv.classList.add('actions');
+          item.appendChild(idDiv);
+          item.appendChild(fioDiv);
+          item.appendChild(createDateDiv);
+          item.appendChild(changeDateDiv);
+          item.appendChild(contactsDiv);
+          item.appendChild(actionsDiv);
+  
+          changeUser.addEventListener('click',() => {
+            changeUserFn(allClients[i].id)
+            document.getElementById('cancle_add').classList.add('dn')
+            document.getElementById('del_user_add').classList.remove('dn')
+            document.getElementById('popup_title').textContent = `Изменить данные `
+            document.getElementById('for_id').textContent = `ID: ${allClients[i].id}`
+            if(document.querySelectorAll('.contact_block_add').length <= 9){
+              document.getElementById('add_contact').classList.remove('dn')
+            }
           })
-        })
-        list.append(item);
-
-        for (let l = 4; l < document.querySelectorAll('.list_item')[i].querySelectorAll('.contact_img_block').length; l++) {
-          document.querySelectorAll('.list_item')[i].querySelectorAll('.contact_img_block')[l].classList.add('dn');
-        }
-
-        const openFullContacts = document.createElement('span')
-        if(allClients[i].contacts.length > 4){
-          openFullContacts.textContent = `+${allClients[i].contacts.length - 4}`
-        }
-
-        openFullContacts.addEventListener('click',() => {
-          const contactImages = document.querySelectorAll(`.list_item:nth-child(${i + 1}) .contact_img_block img`);
-          const spans = document.querySelectorAll(`.list_item:nth-child(${i + 1}) .contacts span`);
-          spans[0].classList.add('dn')
-          document.querySelectorAll('.list_item')[i].querySelectorAll('.contact_img_block').forEach(el => el.classList.remove('dn'))
-          for (let j = 4; j < allClients[i].contacts.length; j++) {
-            contactImages[j].classList.remove('dn');
-            contactImages[j].classList.add('margin_for_all_contacts')
+  
+          deleteUser.addEventListener('click',() => {
+            delPopup.classList.remove('dn')
+            document.getElementById('del_btn').addEventListener('click',() => {
+              deleteUserFromBase(allClients[i].id)
+            })
+          })
+          list.append(item);
+  
+          for (let l = 4; l < document.querySelectorAll('.list_item')[i].querySelectorAll('.contact_img_block').length; l++) {
+            document.querySelectorAll('.list_item')[i].querySelectorAll('.contact_img_block')[l].classList.add('dn');
           }
-        });    
-
-        openFullContacts.textContent === '' ? openFullContacts.classList.add('dn') : false //? скрываем пустой шарик
-        contactsDiv.append(openFullContacts)
+  
+          const openFullContacts = document.createElement('span')
+          if(allClients[i].contacts.length > 4){
+            openFullContacts.textContent = `+${allClients[i].contacts.length - 4}`
+          }
+  
+          openFullContacts.addEventListener('click',() => {
+            const contactImages = document.querySelectorAll(`.list_item:nth-child(${i + 1}) .contact_img_block img`);
+            const spans = document.querySelectorAll(`.list_item:nth-child(${i + 1}) .contacts span`);
+            spans[0].classList.add('dn')
+            document.querySelectorAll('.list_item')[i].querySelectorAll('.contact_img_block').forEach(el => el.classList.remove('dn'))
+            for (let j = 4; j < allClients[i].contacts.length; j++) {
+              contactImages[j].classList.remove('dn');
+              contactImages[j].classList.add('margin_for_all_contacts')
+            }
+          });    
+  
+          openFullContacts.textContent === '' ? openFullContacts.classList.add('dn') : false //? скрываем пустой шарик
+          contactsDiv.append(openFullContacts)
+        }
       }
     }
   } catch (err) {
